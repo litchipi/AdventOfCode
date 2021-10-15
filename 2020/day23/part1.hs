84d7fe1843720b915cddb012ad43fdf747ef5b0a
Timothee Cercueil <timothee.cercueil@tweag.io>
2021-10-15 12:11:13 +0200

    day23 part1: Implement cup manipulation basis
    Signed-off-by: Timothee Cercueil <timothee.cercueil@tweag.io>

--- 2020/day23/part1.hs
+++ 2020/day23/part1.hs
@@ -35,6 +35,8 @@ get_result_from_state state = map cup_to_char (circle state)
 
 
 
+
+
 pick_cups :: CupCircle -> Int -> Int -> [Cup]
 pick_cups list ind len 
   | (ind + len) > (length list) = (pick_cups list ind ((length list) - ind)) ++ take ((ind + len) - (length list)) list
@@ -45,6 +47,18 @@ pop_circle list ind len
   | (ind + len) > (length list) = drop ((ind + len) - (length list)) (pop_circle list ind ((length list) - ind))
 pop_circle list ind len = (take ind list) ++ (drop (ind + len) list)
 
+choose_destination :: Cup -> [Cup] -> (Int, Int) -> Cup
+choose_destination dest picked range
+  | dest < (fst range) = choose_destination ((snd range) - 1) picked range
+  | elem dest picked = choose_destination (dest - 1) picked range
+  | otherwise = dest
+
+find_index :: Cup -> [Cup] -> Int
+find_index val list = undefined
+
+insert_circle :: Int -> [Cup] -> CupCircle -> CupCircle
+insert_circle ind cups circle = undefined
+
 compute_game :: Int -> GameState -> GameState
 compute_game nrounds state | Debug.trace ("Round " ++ (show nrounds) ++ " of game, state: " ++ (show state)) False = undefined
 compute_game nrounds state = final_state
@@ -52,11 +66,17 @@ compute_game nrounds state = final_state
 		cups_picked = pick_cups (circle state) ((current_cup_index state) + 1) 3
 		circle_popped = pop_circle (circle state) ((current_cup_index state) + 1) 3
 
-		-- TODO		Perform modifications on state here
-		new_circle = Debug.trace ("Circle popped: " ++ (show circle_popped)) circle_popped
-			`seq` Debug.trace ("Cups picked: " ++ (show cups_picked)) cups_picked
-			`seq` (circle state) 
-		new_state = GameState { current_cup_index = (current_cup_index state) + 1, circle = new_circle }
+		current_cup = (circle state) !! (current_cup_index state)
+		range = (minimum (circle state), maximum (circle state))
+		destination = choose_destination (current_cup - 1) cups_picked range
+
+		destination_index = find_index destination circle_popped
+		new_circle = insert_circle destination_index cups_picked circle_popped
+
+		new_currentcup_index = find_index current_cup new_circle
+		next_currentcup_index = new_currentcup_index + 1
+
+		new_state = GameState { current_cup_index = next_currentcup_index, circle = new_circle }
 		final_state = if nrounds == 0
 				   then new_state
 				   else compute_game (nrounds - 1) new_state
