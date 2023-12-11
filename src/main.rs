da35cf1359ce2fe44dd4d425e01bacfe22361fb6
Litchi Pi <litchi.pi@proton.me>
2023-12-11 18:31:30 +0100

    Update
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,35 @@
 mod day1;
 mod day2;
 
-const AVG_SOLVE_TIME_LIMIT: u32 = 100_000;
+const AVG_SOLVE_TIME_LIMIT: u32 = 200_000;
 
 fn main() {
-    let res = day2::solve();
+    let f = day2::solve;
+    let res = f();
     println!("=== RESULT ===");
     println!("{res:?}");
 
     #[cfg(not(debug_assertions))]
-    benchmark();
+    benchmark(|| { let _ = f(); });
 }
 
-fn benchmark() {
+fn benchmark<F: FnOnce() + Copy>(f: F) {
     let mut solve_t = std::time::Duration::from_secs(0);
-    let mut coeff = 0;
-    loop {
-        if coeff > AVG_SOLVE_TIME_LIMIT {
-            println!("\n");
-            break;
-        }
 
+    // Warmup
+    for _ in 0..(AVG_SOLVE_TIME_LIMIT / 1_000) {
+        f();
+    }
+
+    for coeff in 0..AVG_SOLVE_TIME_LIMIT {
         let tstart = std::time::Instant::now();
-        day2::solve();
+        f();
         let elapsed = tstart.elapsed();
 
         solve_t = ((solve_t * coeff) + elapsed) / (coeff + 1);
-        coeff += 1;
         if coeff % (AVG_SOLVE_TIME_LIMIT / 1000) == 0 {
             print!("\rAverage elapsed time: {solve_t:?} ({coeff} runs)");
         }
     }
+    println!("\n");
 }
